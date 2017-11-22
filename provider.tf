80c38d9e795fd2a748439601c2ad96626c1c4df7
Twana Daniel <twanaazi@ie.ibm.com>
2017-11-22 14:52:58 +0000

    Add obejct storage

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ variable "softlayer_api_key" {
   description = "Your Bluemix Infrastructure (SoftLayer) API key."
 }
 
+resource "ibm_object_storage_account" "IaC-object-storage" {
+  softlayer_username = "${var.softlayer_username}"
+  softlayer_api_key = "${var.softlayer_api_key}"
+}
+
 provider "ibm" {
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key = "${var.softlayer_api_key}"
